0946f67477a9bd34ea26b4887cea9282191b8854
faizadli <mochfaizadli@gmail.com>
2021-04-16 13:16:11 +0700

    Initial Commit

---
+++ MealAPI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MealAPI
+//
+//  Created by faiz on 16/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
